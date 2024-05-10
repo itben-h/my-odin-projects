c6f43fde7ceaa82be1fca2ce84583cc778533cb9
itben-h <67160296+itben-h@users.noreply.github.com>
2024-05-10 23:07:28 +0800

    Add mouseover

--- foundations/04-etch-a-sketch/javascript.js
+++ foundations/04-etch-a-sketch/javascript.js
@@ -4,6 +4,7 @@ const sqMaster = document.createElement('div');
 
 rowMaster.style.display = 'flex';
 rowMaster.style.justifyContent = 'center';
+sqMaster.classList.add('sq');
 sqMaster.style.cssText = 'height: 2em; width: 2em; border-style: solid';
 
 container.appendChild(rowMaster);
@@ -16,3 +17,10 @@ for (let i = 0; i < 16; i++) {
     let row = rowMaster.cloneNode(true);
     container.appendChild(row);
 }
+
+const sqAll = document.getElementsByClassName('sq');
+Array.from(sqAll).forEach((sq) => {
+    sq.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'red';
+    });
+});
