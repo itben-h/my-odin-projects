575e70cd73fb5cc3898981d593c8aa8c68d5c9bb
itben-h <67160296+itben-h@users.noreply.github.com>
2024-05-10 17:51:08 +0800

    Edit some code

---
+++ foundations/04-etch-a-sketch/javascript.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('#container');
+const row = document.createAttribute('div');
+const sq = document.createAttribute('div');
+
+row.style.display = 'flex'
+sq.style.cssText = 'border-style: solid';
+
+for (let i = 0; i < 16; i++) {
+    container.append
+}
