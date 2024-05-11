aa75559e87c0452b7ce1c3b2a400fa11d209c8c8
itben-h <67160296+itben-h@users.noreply.github.com>
2024-05-11 23:30:30 +0800

    Added grid resizing and resetting

--- foundations/04-etch-a-sketch/javascript.js
+++ foundations/04-etch-a-sketch/javascript.js
@@ -1,30 +1,67 @@
 const container = document.querySelector('#container');
+const gridChange = document.querySelector('#gridChange');
+const gridReset = document.querySelector('#gridReset');
+const update = document.querySelector('#update');
 const rowMaster = document.createElement('div');
 const sqMaster = document.createElement('div');
+const message = document.createElement('div');
 
-let length = container.clientWidth;
-let divisor = 16;
-length = length / divisor;
-
+// Grid Builder
 rowMaster.style.display = 'flex';
 rowMaster.style.justifyContent = 'center';
 sqMaster.classList.add('sq');
-sqMaster.style.cssText = `height: ${length}px; width: ${length}px`;
 
-container.appendChild(rowMaster);
-for (let i = 0; i < divisor; i++) {
-    let sq = sqMaster.cloneNode();
-    rowMaster.appendChild(sq);
-}
+function createGrid(divisor = 16) {
+    let length = container.clientWidth;
+    length = length / divisor;
+    sqMaster.style.cssText = `height: ${length}px; width: ${length}px`;
+    
+    container.appendChild(rowMaster);
+    for (let i = 0; i < divisor; i++) {
+        let sq = sqMaster.cloneNode();
+        rowMaster.appendChild(sq);
+    }
 
-for (let i = 0; i < divisor - 1; i++) {
-    let row = rowMaster.cloneNode(true);
-    container.appendChild(row);
-}
+    for (let i = 0; i < divisor - 1; i++) {
+        let row = rowMaster.cloneNode(true);
+        container.appendChild(row);
+    }
 
-const sqAll = document.getElementsByClassName('sq');
-Array.from(sqAll).forEach((sq) => {
-    sq.addEventListener('mouseover', (event) => {
-        event.target.style.backgroundColor = 'red';
+    document.querySelectorAll('.sq').forEach((sq) => {
+        sq.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = 'red';
+        });
     });
+}
+
+createGrid();
+
+// Button Events
+function updateMessage(str) {
+    message.textContent = str;
+    update.appendChild(message);
+    setTimeout(() => update.removeChild(update.firstChild), 1500);
+}
+
+gridChange.addEventListener('click', () => {   
+    userNum = Number(prompt('Input a new grid size (min 1, max 100)'));
+    if (userNum >= 1 && userNum <= 100) {
+        document.querySelectorAll('.sq').forEach((sq) => {
+            sq.remove();
+        });
+        while (container.firstChild) 
+            container.removeChild(container.firstChild);
+        createGrid(userNum);
+        updateMessage('Grid Resized!');
+    }
+    else {
+        updateMessage('Error! Try Again.');
+    }
+});
+
+gridReset.addEventListener('click', () => {
+    document.querySelectorAll('.sq').forEach((sq) => {
+        sq.style.backgroundColor = 'initial';
+    });
+    updateMessage('Grid Reset!');
 });
